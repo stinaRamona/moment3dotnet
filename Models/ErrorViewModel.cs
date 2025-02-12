e5ffbd6020ed9ce8a2c97cc626e02938a849aeae
Stina Persson <stpe1901@student.miun.se>
2025-02-12 16:44:29 +0100

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace moment3dotnet.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
